0d2c13bba57fdddac43dc32506ba31de468cb08e
MoistyMoist <mr.lawrance@outlook.com>
2013-11-06 02:51:59 +0800

    perfect service left api library

--- PetSocietyWebServices/PetSocietyWebServices/Controllers/ValuesController.cs
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/ValuesController.cs
@@ -14,12 +14,6 @@ namespace PetSocietyWebServices.Controllers
     {
 
 
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
         [HttpGet]
         public List<USER> GetTest()
         {
@@ -35,19 +29,6 @@ namespace PetSocietyWebServices.Controllers
               return errors;
             }
         }
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+       
     }
 }
