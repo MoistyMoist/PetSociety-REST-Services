4a09ae78134e7ab77313ad6260105024a1129dad
MoistyMoist <mr.lawrance@outlook.com>
2013-11-06 01:59:17 +0800

    added REST model objects

---
+++ PetSocietyWebServices/PetSocietyWebServices/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PetSocietyWebServices;
+
+namespace PetSocietyWebServices.Models
+{
+    public class UserModel
+    {
+
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public List<USER> Data { get; set; }
+        public List<string> ErrorList { get; set; }
+
+
+        public UserModel()
+        {
+            this.Status = 0;
+            this.Message = "";
+            this.Data = null;
+            this.ErrorList = null;
+        }
+    }
+}
