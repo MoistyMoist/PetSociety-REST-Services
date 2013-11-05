e32254e5db5c1a1fe20f5ed4ad980a5313515b58
MoistyMoist <mr.lawrance@outlook.com>
2013-11-06 02:15:55 +0800

    added a few test web service controller

---
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PetSocietyWebServices.Controllers.AccountControls
+{
+    public class RegisterController : ApiController
+    {
+        // GET api/register
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/register/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/register
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/register/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/register/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/UpdateController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PetSocietyWebServices.Controllers.AccountControls
+{
+    public class UpdateController : ApiController
+    {
+        // GET api/update
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/update/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/update
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/update/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/update/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
