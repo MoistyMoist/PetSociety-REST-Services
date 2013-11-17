0fcaa56680a2d9a6bd888cb2cf3c0a3e86eb77f5
MoistyBurger <kaiquan88@gmail.com>
2013-11-18 01:45:48 +0800

    removed reduncicy

--- PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/UpdateController.cs
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/UpdateController.cs
@@ -9,31 +9,6 @@ namespace PetSocietyWebServices.Controllers.AccountControls
 {
     public class UpdateController : ApiController
     {
-        // GET api/update
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/update/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/update
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/update/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/update/5
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
