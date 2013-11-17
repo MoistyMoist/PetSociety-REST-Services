bd40273d7c421948ad3e31a58679b9e4dba128f1
MoistyBurger <kaiquan88@gmail.com>
2013-11-18 04:16:44 +0800

    research how to upload image n clean up some controllers dor euru part

---
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/ResetPasswordController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PetSocietyWebServices.Controllers.AccountControls
+{
+    public class ResetPasswordController : ApiController
+    {
+        [HttpGet]
+        public String ResetPassword(String UserID, String INemail, String INpassword)
+
+        {
+            //add a new table to store variaction code to a user
+            return "password updated";
+        }
+    }
+}
--- PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/RetrieveUserController.cs
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/RetrieveUserController.cs
@@ -4,36 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Net.Mail;
+using System.Data.Entity.Validation;
+using PetSocietyWebServices.Models;
+
 
 namespace PetSocietyWebServices.Controllers.AccountControls
 {
     public class RetrieveUserController : ApiController
     {
-        // GET api/retrievealluser
-        public IEnumerable<string> Get()
+        [HttpGet]
+        public UserModel RetrieveUsers(String token)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/retrievealluser/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/retrievealluser
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/retrievealluser/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/retrievealluser/5
-        public void Delete(int id)
-        {
+            return new UserModel();
         }
     }
 }
