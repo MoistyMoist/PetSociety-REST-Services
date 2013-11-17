8e961194f31906de5f9b56d0302db109b0b1f886
MoistyBurger <kaiquan88@gmail.com>
2013-11-18 03:00:35 +0800

    s

--- PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/RetrieveImageController.cs
+++ PetSocietyWebServices/PetSocietyWebServices/Controllers/AccountControls/RetrieveImageController.cs
@@ -4,36 +4,55 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Net.Mail;
+using PetSocietyWebServices.Models;
 
 namespace PetSocietyWebServices.Controllers.AccountControls
 {
     public class RetrieveImageController : ApiController
     {
-        // GET api/retrieveimage
-        public IEnumerable<string> Get()
+        [HttpGet]
+        public ImageModel retrieveImage(String token, int INimageID, int INuserID, int INpetID)
         {
-            return new string[] { "value1", "value2" };
-        }
+            using (PetSocietyDBEntities db = new PetSocietyDBEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                //LOAD THE QUERY
+                var query = from c in db.IMAGEs
+                            where (c.GalleryID == INimageID || c.UserID == INuserID || c.PetID == INpetID)
+                            select c;
 
-        // GET api/retrieveimage/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/retrieveimage
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/retrieveimage/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/retrieveimage/5
-        public void Delete(int id)
-        {
+                //CONVERT THE RESULT TO A LIST
+                List<IMAGE> OUTImage = query.ToList();
+                if (token.Equals("token"))
+                {
+                    if (OUTImage.Count() == 0)
+                    {
+                        ImageModel model = new ImageModel();
+                        model.Status = 1;
+                        model.Message = "No such image exists. please create one if needed or check again";
+                        return model;
+                    }
+                    else
+                    {
+                        ImageModel model = new ImageModel();
+                        model.Status = 0;
+                        model.Message = "Retrieve success";
+                        model.Data = OUTImage;
+                        return model;
+                    }
+                }
+                else
+                {
+                    ImageModel model = new ImageModel();
+                    model.Status = 1;
+                    model.Message = "Token error, invalid token";
+                    return model;
+                }
+               
+            }
         }
     }
 }
